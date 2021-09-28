3dd010284a696217acc2aedbb0250e78b2455167
Rafael Sanzio <rafaelsanzio16@gmail.com>
2021-09-28 16:31:55 -0300

    removendo cursos antigos e aprendendo sobre taskscheduller

--- paralelismo-csharp/ByteBank.View/MainWindow.xaml.cs
+++ paralelismo-csharp/ByteBank.View/MainWindow.xaml.cs
@@ -34,7 +34,43 @@ namespace ByteBank.View
 
         private void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
+            var taskSchedullerUi = TaskScheduler.FromCurrentSynchronizationContext(); //task principal
+            BtnProcessar.IsEnabled = false; // no inicio do processamento desativa o btn
+
             var contas = r_Repositorio.GetContaClientes();
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+
+            var contasTarefas = contas.Select(conta =>
+            {
+                return Task.Factory.StartNew(() => //o factory demanda as tarefas para as taskScheduller, ele gerencia!
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                });
+            }).ToArray(); // o to Array é necessario para "obrigar" o select(linq) a efetuar o processo para todas as contas
+
+            //Task.WaitAll(contasTarefas); // espera o processamento de todas as tarefas(contas a ser processadas) serem concluidas MAS temos o problema da interface ficar congelada
+
+            Task.WhenAll(contasTarefas) // resolvendo o problema de congelar a UI pois o whenAll executa apos finalizar uma tarefa
+                .ContinueWith(task =>
+                {
+                    var fim = DateTime.Now;
+                    AtualizarView(resultado, fim - inicio);
+                }, taskSchedullerUi) // executa de acordo com a demanda taskSchedullerUi definido na linha 2 e resolve o erro da excecao de threadh nao permitida pois o InvalidOperationException, ocorre quando a tentativa de se acessar um objeto da Thread da interface gráfica a partir de outra Thread
+                .ContinueWith(task =>
+                {
+                    BtnProcessar.IsEnabled = true; //ativa o btn apos o processamento
+                });
+
+            /* codigo antigo - THREADS
+             
+            neste momento nao estamos trabalhando com o gerenciador de tarefas, estamos definindo individualmente
+            as quantidade de tarefas por treahds
 
             var contasQuantidadePorThread = contas.Count() / 4; // como vou usar 4 threahds vou dividir em porcao de 4
             var contas_parte1 = contas.Take(contasQuantidadePorThread);
@@ -43,12 +79,6 @@ namespace ByteBank.View
             var contas_parte4 = contas.Skip(contasQuantidadePorThread * 3);
             //var contas_parte1 = contas.Take(contas.Count() / 2); //pega a primeira metade da lista
             //var contas_parte2 = contas.Skip(contas.Count() / 2); // pula pra metade e pega essa metade
-
-            var resultado = new List<string>();
-
-            AtualizarView(new List<string>(), TimeSpan.Zero);
-
-            var inicio = DateTime.Now;
 
             Thread thread_parte1 = new Thread(() => //processa a primeira metade da lista
             {
@@ -90,17 +120,12 @@ namespace ByteBank.View
             thread_parte2.Start();
             thread_parte3.Start();
             thread_parte4.Start();
-
-
             while (thread_parte1.IsAlive || thread_parte2.IsAlive || thread_parte3.IsAlive || thread_parte4.IsAlive) // verifica se as thread estao em execucao (isAlive)
             {
                 Thread.Sleep(250); // ao inves de perguntar SEMPRE vamos perguntar uma vez e esperar o tempo definido com isso diminuimos 20s do processamento
             }
+             */
 
-
-            var fim = DateTime.Now;
-
-            AtualizarView(resultado, fim - inicio);
         }
 
         private void AtualizarView(List<String> result, TimeSpan elapsedTime)
@@ -167,6 +192,39 @@ Console.WriteLine(isAlive);
 
 Qual será o valor booleano de isAlive?
 R: False. Dado que a thread não foi iniciada com o método Start, sua propriedade IsAlive nunca será true
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+Do ponto de vista de performance, em um processador quad-core, qual a diferença entre os códigos abaixo?
+
+1.
+for(int i = 0; i < 100; i++) {
+    var msg = "Thread número " + i;
+    var thread = new Thread(() => Console.WriteLine(msg));
+        thread.Start();
+}
+
+2.
+for(int i = 0; i < 100; i++) {
+    var msg = "Task número " + i;
+    Task.Factory.StartNew(() => Console.WriteLine(msg));
+}
+
+A solução em 2. é mais performática, porque passamos a responsabilidade de provisionamento e gerenciamento das Threads disponíveis para o TaskScheduler default usado pela Task.Factory, que possui uma inteligência
+O uso da Task.Factory e o default TaskScheduler é uma boa prática pois o gerenciamento das threads por eles é feito de forma muito inteligente
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+Você é usuário de uma biblioteca que possui um método com a assinatura Task IniciarDownload() e quer executar um método chamado FimDownload() ao término dela, mantendo a UI responsiva. O código atual é:
+
+var download = new Download(url);
+download.IniciarDownload();
+
+Qual alternativa reflete a correta implementação para atender este requisito?
+
+download.IniciarDownload().ContinueWith(() => FimDownload());
+
+
+Correta. ContinueWith é o método correto que nos permite encadear a execução de uma tarefa em outra
 
 */
