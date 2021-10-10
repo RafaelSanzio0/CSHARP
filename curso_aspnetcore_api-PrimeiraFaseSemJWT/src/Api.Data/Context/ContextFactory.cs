16bfc7bbfafca43b1d14bc825b96f1a97f38e6a2
Rafael Sanzio <rafaelsanzio16@gmail.com>
2021-10-10 20:10:40 -0300

    56 - Api.data - criar contextFactory

--- curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Data/Context/ContextFactory.cs
+++ curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Data/Context/ContextFactory.cs
@@ -4,13 +4,11 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace Api.Data.Context {
     public class ContextFactory : IDesignTimeDbContextFactory<MyContext> {
         public MyContext CreateDbContext (string[] args) {
-            //Usado para Criar as Migrações
-            //var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123";
-            var connectionString = "Server=.\\SQLEXPRESS2017;Database=dbAPI;User Id=sa;Password=mudar@123";
-            var optionsBuilder = new DbContextOptionsBuilder<MyContext> ();
-            //optionsBuilder.UseMySql (connectionString);
-            optionsBuilder.UseSqlServer (connectionString);
-            return new MyContext (optionsBuilder.Options);
+            //Usado para Criar as Migrações (banco de dados,tabela)
+            var connectionString = "server=localhost;database=CinemaDB;user=root;password=root";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
         }
     }
 }
