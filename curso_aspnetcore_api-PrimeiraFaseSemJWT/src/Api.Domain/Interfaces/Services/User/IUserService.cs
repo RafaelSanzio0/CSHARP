379e865dec02c4678228642bec19b96d8e83eb13
Rafael Sanzio <rafaelsanzio16@gmail.com>
2021-10-14 19:51:14 -0300

    65,66 e 67 - api.service desenvolvimento da camada de servico app > service > domain > data

--- curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Domain/Interfaces/Services/User/IUserService.cs
+++ curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -5,10 +5,12 @@ using Api.Domain.Entities;
 
 namespace Api.Domain.Interfaces.Services.User {
     public interface IUserService {
-        Task<UserEntity> Get (Guid id);
-        Task<IEnumerable<UserEntity>> GetAll ();
-        Task<UserEntity> Post (UserEntity user);
-        Task<UserEntity> Put (UserEntity user);
-        Task<bool> Delete (Guid id);
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(Guid id);
     }
 }
+// deixar generico futuramente
+// a camada de aplication que trabalha com esses metodos da servie
