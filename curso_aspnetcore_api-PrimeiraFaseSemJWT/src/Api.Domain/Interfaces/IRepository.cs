812139a692f34f05c4a3597286a6502474be39a6
Rafael Sanzio <rafaelsanzio16@gmail.com>
2021-10-14 14:20:00 -0300

    59 - api.Doamin criando a interface de repository

--- curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Domain/Interfaces/IRepository.cs
+++ curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Domain/Interfaces/IRepository.cs
@@ -4,12 +4,12 @@ using System.Threading.Tasks;
 using Api.Domain.Entities;
 
 namespace Api.Domain.Interfaces {
-    public interface IRepository<T> where T : BaseEntity {
+    public interface IRepository<T> where T : BaseEntity { // vai ser injetado aqui qualquer classe onde tem como heranca o baseEntity
         Task<T> InsertAsync (T item);
         Task<T> UpdateAsync (T item);
         Task<bool> DeleteAsync (Guid id);
         Task<T> SelectAsync (Guid id);
-        Task<IEnumerable<T>> SelectAsync ();
+        Task<IEnumerable<T>> SelectAsync (); //Ienumerable tem menos funcoes porem e mais rapido
         Task<bool> ExistAsync (Guid id);
     }
 }
