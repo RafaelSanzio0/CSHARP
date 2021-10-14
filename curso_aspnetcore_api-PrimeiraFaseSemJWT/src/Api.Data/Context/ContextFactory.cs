ae0c4998b879924a9d45de2d01627cf97d732fe6
Rafael Sanzio <rafaelsanzio16@gmail.com>
2021-10-14 14:02:00 -0300

    criando migracoes com o bd

--- curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Data/Context/ContextFactory.cs
+++ curso_aspnetcore_api-PrimeiraFaseSemJWT/src/Api.Data/Context/ContextFactory.cs
@@ -5,7 +5,7 @@ namespace Api.Data.Context {
     public class ContextFactory : IDesignTimeDbContextFactory<MyContext> {
         public MyContext CreateDbContext (string[] args) {
             //Usado para Criar as Migrações (banco de dados,tabela)
-            var connectionString = "server=localhost;database=CinemaDB;user=root;password=root";
+            var connectionString = "server=localhost;database=UdemyDB;user=root;password=root";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
