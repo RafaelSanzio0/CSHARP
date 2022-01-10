f45e9e1b0564d69afa3792e2bb9313e04569afab
Rafael Sanzio <rafaelsanzio16@gmail.com>
2022-01-10 00:08:22 -0300

    tecnicas de refatoracao

---
+++ Refatoramento/Refatoracao 2/Refatorando2/Aula1/R19.ReplaceArrayWithObject/antes/Empresa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace refatoracao.R19.ReplaceArrayWithObject.antes
+{
+    class Empresa
+    {
+        public Empresa()
+        {
+            using (var streamReader = File.OpenText("clientes.csv"))
+            {
+                string linha = string.Empty;
+                while ((linha = streamReader.ReadLine()) != null)
+                {
+                    string[] campos = linha.Split(',');
+
+                    Cliente cliente = new Cliente(campos[0], campos[1], campos[2], campos[4]);
+
+                    Console.WriteLine("Dados do Cliente");
+                    Console.WriteLine("================");
+                    Console.WriteLine("ID: " + cliente.Id);
+                    Console.WriteLine("Nome: " + cliente.Nome);
+                    Console.WriteLine("Telefone: " + cliente.Telefone);
+                    Console.WriteLine("Website: " + cliente.WebSite);
+                    Console.WriteLine("================");
+                }
+            }
+        }
+    }
+
+    class Cliente
+    {
+        private readonly string id;
+        private readonly string nome;
+        private readonly string telefone;
+        private readonly string webSite;
+
+        public string Id => id;
+
+        public string Nome => nome;
+
+        public string Telefone => telefone;
+
+        public string WebSite => webSite;
+
+        public Cliente(string id, string nome, string telefone, string webSite)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.telefone = telefone;
+            this.webSite = webSite;
+        }
+    }
+}
---
+++ Refatoramento/Refatoracao 2/Refatorando2/Aula3/R28.ReplaceTypeCodeWithClass/depois/ContaBancaria.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace refatoracao.R28.ReplaceTypeCodeWithClass.depois
+{
+    class Programa
+    {
+        public void Main()
+        {
+            var minhaContaCorrente = new ContaBancaria(TipoConta.ContaCorrente(), 100m);
+            var minhaPoupanca = new ContaBancaria(TipoConta.Poupanca(), 300m);
+            var meuInvestimento = new ContaBancaria(TipoConta.Investimento(), 1500m);
+            //var teste = new ContaBancaria(5, 1239m); da erro entao nao é possivel passar qualquer valor
+
+
+            minhaContaCorrente.Depositar(100);
+            minhaContaCorrente.Sacar(75);
+
+            minhaPoupanca.Depositar(55);
+            minhaPoupanca.Sacar(16);
+
+            meuInvestimento.Depositar(40);
+            meuInvestimento.Sacar(30);
+        }
+    }
+
+    class ContaBancaria
+    {
+        private readonly TipoConta tipoConta;
+        public TipoConta TipoConta
+        {
+            get
+            {
+                return tipoConta;
+            }
+        }
+
+        private decimal saldo;
+        public decimal Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+        }
+
+        public ContaBancaria(TipoConta tipoConta, decimal saldoInicial)
+        {
+            this.tipoConta = tipoConta;
+            this.saldo = saldoInicial;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new ArgumentException("Saldo insuficiente");
+            }
+
+            this.saldo -= valor;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            this.saldo += valor;
+        }
+    }
+
+    class TipoConta
+    {
+        public static int CONTA_CORRENTE = 0;
+        public static int POUPANCA = 1;
+        public static int INVESTIMENTO = 2;
+
+        private readonly int id;
+
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        private TipoConta(int id)
+        {
+            this.id = id;
+        }
+
+        public static TipoConta ContaCorrente()
+        {
+            return new TipoConta(CONTA_CORRENTE);
+        }
+
+        public static TipoConta Poupanca()
+        {
+            return new TipoConta(POUPANCA);
+        }
+
+        public static TipoConta Investimento()
+        {
+            return new TipoConta(INVESTIMENTO);
+        }
+    }
+}
