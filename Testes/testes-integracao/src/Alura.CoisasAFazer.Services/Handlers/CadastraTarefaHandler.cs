d5f88a39443f0cc73d5bfdc49c7c18679701504c
Rafael Sanzio <rafaelsanzio16@gmail.com>
2022-04-27 01:44:52 -0300

    fnializando curso d testes d integração com xunit e moq

--- Testes/testes-integracao/src/Alura.CoisasAFazer.Services/Handlers/CadastraTarefaHandler.cs
+++ Testes/testes-integracao/src/Alura.CoisasAFazer.Services/Handlers/CadastraTarefaHandler.cs
@@ -11,25 +11,34 @@ namespace Alura.CoisasAFazer.Services.Handlers
         IRepositorioTarefas _repo;
         ILogger<CadastraTarefaHandler> _logger;
 
-        public CadastraTarefaHandler(IRepositorioTarefas repositorio)
+        public CadastraTarefaHandler(IRepositorioTarefas repositorio, ILogger<CadastraTarefaHandler> logger)
         {
             _repo = repositorio;
-            _logger = new LoggerFactory().CreateLogger<CadastraTarefaHandler>();
+            _logger = logger;
         }
 
-        public void Execute(CadastraTarefa comando)
+        public CommandResult Execute(CadastraTarefa comando)
         {
-            var tarefa = new Tarefa
-            (
-                id: 0,
-                titulo: comando.Titulo,
-                prazo: comando.Prazo,
-                categoria: comando.Categoria,
-                concluidaEm: null,
-                status: StatusTarefa.Criada
-            );
-            _logger.LogDebug("Persistindo a tarefa...");
-            _repo.IncluirTarefas(tarefa);
+            try
+            {
+                var tarefa = new Tarefa
+                (
+                    id: 0,
+                    titulo: comando.Titulo,
+                    prazo: comando.Prazo,
+                    categoria: comando.Categoria,
+                    concluidaEm: null,
+                    status: StatusTarefa.Criada
+                );
+                _logger.LogDebug($"Persistindo a tarefa {tarefa.Titulo}");
+                _repo.IncluirTarefas(tarefa);
+                return new CommandResult(true);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return new CommandResult(false);
+            }
         }
     }
 }
